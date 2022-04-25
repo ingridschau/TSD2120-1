0d442e51aa77399dc350cd3071a7bb25ce196e30
Ingrid Schau <ingrid.te.schau@gmail.com>
2022-04-25 10:33:52 +0200

    Added error-handling

--- oblig-2/oblig2.c
+++ oblig-2/oblig2.c
@@ -9,49 +9,22 @@ int balance = 0;
 pthread_mutex_t mutex;
 FILE *fpIncome, *fpExpense;
 
-void *readIncome(void *args)
-{
-	int income = 0;
-
-	fpIncome = fopen("inn.txt", "r");
-	
-	//iterate through the file, line by line
-	while(fscanf(fpIncome, "%d", &income)==1)
-	{
-		pthread_mutex_lock(&mutex);
-		balance = balance + income;
-		pthread_mutex_unlock(&mutex);
-	}
-	fclose(fpIncome);
-}
-
-void *readExpense(void *args)
-{
-	int expense = 0;
-
-	fpExpense = fopen("ut.txt", "r");
-
-	//iterare through the file, line by line
-	while(fscanf(fpExpense, "%d", &expense)==1)
-	{
-		//locks mutex, updates the balance, opens mutex
-		pthread_mutex_lock(&mutex);
-		balance = balance - expense;
-		pthread_mutex_unlock(&mutex);
-	}
-	fclose(fpExpense);
-}
+void *readIncome(void *args);
+void *readExpense(void *args);
 
 int main(int argc, char* argv[]){
 
 	pthread_t threads[2];
 	
 	//initializing the mutex memory
-	pthread_mutex_init(&mutex, NULL);
+	if(pthread_mutex_init(&mutex, NULL))
+		printf("Error initialising mutex for balance.\n");
 	
 	//making the threads
-	pthread_create(&threads[0], NULL, &readIncome, NULL);
-	pthread_create(&threads[1], NULL, &readExpense, NULL);
+	if(pthread_create(&threads[0], NULL, &readIncome, NULL))
+		printf("Error creating income listener thread.\n");
+	if(pthread_create(&threads[1], NULL, &readExpense, NULL))
+		printf("Error creating expense listener thread.\n");
 
 	pthread_join(threads[0], NULL);
 	pthread_join(threads[1], NULL);
@@ -64,5 +37,44 @@ int main(int argc, char* argv[]){
 	printf("Remaining balance: %d", balance);
 
 	return 0;
+}
+
+void *readIncome(void *args)
+{
+	int income = 0;
+
+	if(fpIncome = fopen("inn.txt", "r"))
+	{
+		//iterate through the file, line by line
+		while(fscanf(fpIncome, "%d", &income) != EOF)
+		{
+			pthread_mutex_lock(&mutex);
+			balance = balance + income;
+			pthread_mutex_unlock(&mutex);
+		}
+		fclose(fpIncome);
+	}
+	else
+		printf("Error opening file \"inn.txt\".");
+}
+
+void *readExpense(void *args)
+{
+	int expense = 0;
+
+	if(fpExpense = fopen("ut.txt", "r"))
+	{
+		//iterate through the file, line by line
+		while(fscanf(fpExpense, "%d", &expense) != EOF)
+		{
+			//locks mutex, updates the balance, opens mutex
+			pthread_mutex_lock(&mutex);
+			balance = balance - expense;
+			pthread_mutex_unlock(&mutex);
+		}
+		fclose(fpExpense);
+	}else
+		printf("Error opening file \"ut.txt\".");
+
 }
 
